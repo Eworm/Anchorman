8988a02831762547bda33a9acf85fa299ae16fa6
Wout <wout@woutmager.nl>
2019-01-24 14:17:18 +0100

    Checking the cron

--- Anchorman/AnchormanTasks.php
+++ Anchorman/AnchormanTasks.php
@@ -16,16 +16,20 @@ class AnchormanTasks extends Tasks
     public function schedule(Schedule $schedule)
     {
 
-        $feeds_storage  = Storage::files('/site/storage/addons/Anchorman');
+        $schedule->command('anchorman:update')->everyMinute();
+        file_get_contents("https://cronhub.io/ping/20ea29f0-0c79-11e9-bad3-65985bc7f86f");
 
-        foreach ($feeds_storage as $feed) {
 
-            $rem = str_replace('site/storage/addons/Anchorman/', '', $feed);
-            $info = $this->storage->getJson($rem);
-            $int = intval($info['scheduling']);
-
-            $schedule->command('anchorman:update')->cron('*/' . $int . ' * * * * *');
-
-        }
+        // $feeds_storage  = Storage::files('/site/storage/addons/Anchorman');
+        //
+        // foreach ($feeds_storage as $feed) {
+        //
+        //     $rem = str_replace('site/storage/addons/Anchorman/', '', $feed);
+        //     $info = $this->storage->getJson($rem);
+        //     $int = intval($info['scheduling']);
+        //
+        //     $schedule->command('anchorman:update')->cron('*/' . $int . ' * * * * *');
+        //
+        // }
     }
 }
