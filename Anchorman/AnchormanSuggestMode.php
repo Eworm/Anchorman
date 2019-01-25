418cabdfe5c45ebe4f82e1ce8dc1f05caa9c19e2
Wout <wout@woutmager.nl>
2019-01-25 19:56:33 +0100

    ..

--- Anchorman/AnchormanSuggestMode.php
+++ Anchorman/AnchormanSuggestMode.php
@@ -6,6 +6,11 @@ class AnchormanSuggestMode extends AbstractMode
 {
     public function suggestions()
     {
-        
+        // return collect(Fieldset::all())->flatMap(function ($fieldset) {
+        //     return collect($fieldset->inlinedFields())->map(function ($config, $name) {
+        //         $type = array_get($config, 'type', 'text');
+        //         return ['value' => $name, 'text' => $name, 'type' => $type];
+        //     })->filter();
+        // })->sortBy('text')->values()->all();
     }
 }
