65f8d280835c7632aed25c422f091da01b692584
Wout <wout@woutmager.nl>
2019-01-29 16:03:40 +0100

    Changed the suggestmode to a folder.yaml

--- Anchorman/FieldSuggestions.php
+++ Anchorman/FieldSuggestions.php
@@ -2,17 +2,33 @@
 
 namespace Statamic\Addons\Anchorman;
 
+use Statamic\API\Collection;
 use Statamic\API\Fieldset;
 
 class FieldSuggestions
 {
     public function suggestions()
     {
-        return collect(Fieldset::all())->flatMap(function ($fieldset) {
-            return collect($fieldset->inlinedFields())->map(function ($config, $name) {
-                $type = array_get($config, 'type', 'text');
-                return ['value' => $name, 'text' => $name, 'type' => $type];
-            })->filter();
-        })->sortBy('text')->values()->all();
+        // return collect(Fieldset::all())->flatMap(function ($fieldset) {
+        //     return collect($fieldset->inlinedFields())->map(function ($config, $name) {
+        //         $type = array_get($config, 'type', 'text');
+        //         return ['value' => $name, 'text' => $name, 'type' => $type];
+        //     })->filter();
+        // })->sortBy('text')->values()->all();
+
+        $test = Collection::whereHandle('feed');
+        return $test->data();
+        // $test = Fieldset::all();
+        // $test = Collection::handles();
+        // dd($test->data());
+
+        // return collect(Collection::whereHandle('feed'))->flatMap(function ($fieldset) {
+            // return collect($fieldset->inlinedFields())->map(function ($config, $name) {
+            //     $type = array_get($config, 'type', 'text');
+            //     return ['value' => $name, 'text' => $name, 'type' => $type];
+            // })->filter();
+        // })->sortBy('text')->values()->all();
+
+
     }
 }
