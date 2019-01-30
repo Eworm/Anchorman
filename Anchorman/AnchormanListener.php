dfc9a865a1fdf09f075e4617ea5ab0af797f826f
Wout <wout@woutmager.nl>
2019-01-30 21:59:13 +0100

    ..

--- Anchorman/AnchormanListener.php
+++ Anchorman/AnchormanListener.php
@@ -22,8 +22,6 @@ class AnchormanListener extends Listener
     */
     public $events = [
         'cp.nav.created' => 'addNavItems',
-        \Statamic\Events\Data\PublishFieldsetFound::class => 'addFieldsetTab',
-        \Statamic\Events\Data\FindingFieldset::class => 'addFieldsetTab',
         'cp.add_to_head' => 'addToHead'
     ];
 
@@ -31,34 +29,6 @@ class AnchormanListener extends Listener
     {
         $syndication = Nav::item('Syndication')->route('addons.anchorman')->icon('rss');
         $nav->addTo('tools', $syndication);
-    }
-
-    public function addFieldsetTab($event)
-    {
-        if (! in_array($event->type, ['page', 'entry', 'term'])) {
-            return;
-        }
-
-        $fieldset = $event->fieldset;
-        $sections = $fieldset->sections();
-
-        $fields = YAML::parse(File::get($this->getDirectory().'/resources/fieldsets/content.yaml'))['fields'];
-
-        $seoFields = collect($fields['anchorman']['fields'])->map(function ($field, $key) use ($event) {
-            $field['placeholder'] = $this->getPlaceholder($key, $field, $event->data);
-            return $field;
-        })->all();
-
-        $fields['anchorman']['fields'] = $this->translateFieldsetFields($seoFields, 'content');
-
-        $sections['anchorman'] = [
-            'display' => 'SEO',
-            'fields' => $fields
-        ];
-
-        $contents = $fieldset->contents();
-        $contents['sections'] = $sections;
-        $fieldset->contents($contents);
     }
 
     protected function getPlaceholder($key, $field, $data)
