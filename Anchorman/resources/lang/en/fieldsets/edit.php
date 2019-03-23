ad7ca70695ee179a0b61f9e193388f6499dcfea3
Wout <wout@woutmager.nl>
2019-03-23 10:36:52 +0100

    Adding english translations

--- Anchorman/resources/lang/en/fieldsets/edit.php
+++ Anchorman/resources/lang/en/fieldsets/edit.php
@@ -2,22 +2,74 @@
 
 return [
 
+    'settings_section' => 'Settings',
+    'settings_section_instruct' => 'General settings for this feed.',
+
     'url' => 'Url',
-    'url_instruct' => "The url of the rss feed.",
 
     'active' => 'Enabled',
     'active_instruct' => "If disabled, this feed won't check for new articles.",
 
-    'publish' => 'Collection',
-    'publish_instruct' => 'Pick a collection for new articles.',
-
     'scheduling' => 'Schedule',
     'scheduling_instruct' => 'Wait time between updates, in minutes.',
 
-    'status' => 'New articles',
-    'status_instruct' => 'What to do with them.',
+    'status' => 'What to do with new articles',
 
-    'publish_options' => 'Item content',
-    'publish_options_instruct' => 'The feed comes with these fields. Select the entry fields they should use.',
+    'query' => 'Add custom queries',
+
+    'query_grid' => 'Custom queries',
+
+    'content_section' => 'Content',
+    'content_section_instruct' => 'Where should the feed content go?',
+
+    'publish_to' => 'The collection for new articles',
+
+    'content_title' => 'Article title',
+    'content_title_instruct' => 'Choose the name of a field or add a custom name.',
+
+    'content_description' => 'Article description',
+    'content_description_instruct' => 'Choose the name of a field, add a custom name, or disable saving the description.',
+
+    'content_content' => 'Article content',
+    'content_content_instruct' => 'Choose the name of a field, add a custom name, or disable saving the content.',
+
+    'content_permalink' => 'Article permalink',
+    'content_permalink_instruct' => 'Choose the name of a field, add a custom name, or disable saving the permalink.',
+
+    'authors_section' => 'Authors',
+    'authors_section_instruct' => 'Options for article authors.',
+
+    'content_authors' => 'Article authors',
+    'content_authors_instruct' => 'Choose the name of a field, add a custom name, or disable saving the author.',
+
+    'author_options' => 'Article authors',
+    'author_options_instruct' => 'Save the article author as a new user or attribute new articles to an existing one.',
+
+    'content_author' => 'Choose a user',
+    'content_author_instruct' => 'To attribute new articles to.',
+
+    'taxonomies_section' => 'Taxonomies & terms',
+    'taxonomies_section_instruct' => 'Options for article categories.',
+
+    'content_taxonomies' => 'Article categories',
+    'content_taxonomies_instruct' => 'Choose the name of a field, add a custom name, or disable saving categories.',
+
+    'custom_taxonomies' => 'Custom term taxonomy',
+    'custom_taxonomies_instruct' => 'Which taxonomy should I use for custom terms?',
+
+    'custom_tags' => 'Custom terms',
+    'custom_tags_instruct' => 'These terms will be added to each new article.',
+
+    'images_section' => 'Images',
+    'images_section_instruct' => 'Options for article images.',
+
+    'content_thumbnail' => 'Article thumbnail',
+    'content_thumbnail_instruct' => 'Choose the name of a field, add a custom name, or disable saving the thumbnail.',
+
+    'save_images' => 'Save thumbnails',
+    'save_images_instruct' => 'Enable to download article thumbnails.',
+
+    'images_container' => 'Choose an asset container',
+    'images_container_instruct' => 'Article thumbnails will be downloaded and saved in the chosen asset container.',
 
 ];
