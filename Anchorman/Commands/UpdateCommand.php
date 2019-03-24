ebae5190bae85031355d24a84deb441049c1ca5b
Wout <wout@woutmager.nl>
2019-03-24 20:01:03 +0100

    ..

--- Anchorman/Commands/UpdateCommand.php
+++ Anchorman/Commands/UpdateCommand.php
@@ -53,200 +53,195 @@ class UpdateCommand extends Command
             $ignore = array( 'cgi-bin', '.', '..','._' );
             if (!in_array($rem, $ignore) and substr($rem, 0, 1) != '.') {
 
+                $info       = $this->storage->getYaml($rem);
+                $url        = $info['url'];
+                $publish_to = $info['publish_to'][0];
+                $active     = $info['active'];
+
+                // Add the last updated time to the feed info
+                $info['updated'] = time();
+                $this->storage->putYAML($rem, $info);
+
+                if (isset($info['author_options'])) {
+                    $author_options = $info['author_options'];
+                }
+
+                if (isset($info['item_author'])) {
+                    $author = $info['item_author'][0];
+                }
+
+                if (isset($info['images_container'])) {
+                    $assetcontainer = $info['images_container'][0];
+                }
+
+                if (isset($info['save_images'])) {
+                    $save_images = $info['save_images'];
+                }
+
                 $feed = new SimplePie();
                 $feed->set_cache_location(Feed::cache_location());
                 $feed->set_feed_url($url);
-                $success = $feed->init();
+                $feed->init();
                 $this->info('Updating ' . $feed->get_title());
                 $i = 0;
 
-                if ($success)
-                {
+                // Get the chosen taxonomy
+                if (isset($info['custom_taxonomies'])) {
+                    $taxonomy = $info['custom_taxonomies'][0];
+                }
 
-                    $info       = $this->storage->getYaml($rem);
-                    $url        = $info['url'];
-                    $publish_to = $info['publish_to'][0];
-                    $active     = $info['active'];
-
-                    // Add the last updated time to the feed info
-                    $info['updated'] = time();
-                    $this->storage->putYAML($rem, $info);
-
-                    if (isset($info['author_options'])) {
-                        $author_options = $info['author_options'];
+                // Custom terms to the chosen taxonomy
+                if (isset($info['custom_terms'])) {
+                    $tags = $info['custom_terms'];
+                    foreach ($tags as $term) {
+                        $this->info('Adding "' . $term . '" to "' . $taxonomy . '".');
+                        Term::create(slugify($term))
+                            ->taxonomy($taxonomy)
+                            ->save();
                     }
+                }
 
-                    if (isset($info['item_author'])) {
-                        $author = $info['item_author'][0];
+                // Custom queries
+                if (isset($info['query_grid'])) {
+                    $queries = $info['query_grid'];
+                    $newquery = [];
+                    foreach ($queries as $query) {
+                        $newquery[$query['name']] = $query['value'];
                     }
-
-                    if (isset($info['images_container'])) {
-                        $assetcontainer = $info['images_container'][0];
+                    if (strpos($url, '?') !== false) {
+                        $url = $url . '&amp;' . http_build_query($newquery, '', '&amp;');
+                    } else {
+                        $url = $url . '?' . http_build_query($newquery, '', '&amp;');
                     }
+                }
 
-                    if (isset($info['save_images'])) {
-                        $save_images = $info['save_images'];
-                    }
+                // Add items to the chosen collection
+                foreach ($feed->get_items() as $item) {
 
+                    if ($active === true) {
 
-                    // Get the chosen taxonomy
-                    if (isset($info['custom_taxonomies'])) {
-                        $taxonomy = $info['custom_taxonomies'][0];
-                    }
+                        $with = [];
+                        $with[$info['item_title']['value']] = $item->get_title(); // Add the title
+                        $with['item_pubdate'] = $item->get_date(); // Add the pubdate
+                        $slugged = slugify($item->get_title());
 
-                    // Custom terms to the chosen taxonomy
-                    if (isset($info['custom_terms'])) {
-                        $tags = $info['custom_terms'];
-                        foreach ($tags as $term) {
-                            $this->info('Adding "' . $term . '" to "' . $taxonomy . '".');
-                            Term::create(slugify($term))
-                                ->taxonomy($taxonomy)
-                                ->save();
+                        // Item description
+                        if (isset($info['item_description']) && $item->get_description()) {
+                            if ($info['item_description']['source'] != 'disable' && $info['item_description']['value'] != null) {
+                                $with[$info['item_description']['value']] = $item->get_description();
+                            }
                         }
-                    }
 
-                    // Custom queries
-                    if (isset($info['query_grid'])) {
-                        $queries = $info['query_grid'];
-                        $newquery = [];
-                        foreach ($queries as $query) {
-                            $newquery[$query['name']] = $query['value'];
+                        // Item content
+                        if (isset($info['item_content']) && $item->get_content()) {
+                            if ($info['item_content']['source'] != 'disable' && $info['item_content']['value'] != null) {
+                                $with[$info['item_content']['value']] = $item->get_content();
+                            }
                         }
-                        if (strpos($url, '?') !== false) {
-                            $url = $url . '&amp;' . http_build_query($newquery, '', '&amp;');
+
+                        // Item permalink
+                        if (isset($info['item_permalink']) && $item->get_permalink()) {
+                            if ($info['item_permalink']['source'] != 'disable' && $info['item_permalink']['value'] != null) {
+                                $with[$info['item_permalink']['value']] = $item->get_permalink();
+                            }
+                        }
+
+                        // Item authors
+                        if (isset($info['item_authors'])) {
+                            if ($info['item_authors']['source'] != 'disable' && $info['item_authors']['value'] != null) {
+                                if ($author_options == 'create') {
+                                    if ($author = $feed->get_author()) {
+                                        // Create new user
+                                        User::create($author->get_name())
+                                            ->username(slugify($author->get_name()))
+                                            ->save();
+                                    }
+                                } else {
+                                    // Assign to an existing user
+                                    $with[$info['item_authors']['value']] = $author;
+                                }
+                            }
+                        }
+
+                        // Item thumbnails
+                        if ($enclosure = $item->get_enclosure()) {
+                            $enclosure_type = $enclosure->get_type();
+                            $enclosure_link = $enclosure->get_link();
+
+                            if (isset($info['item_thumbnail'])) {
+                                if ($info['item_thumbnail']['source'] != 'disable' && $info['item_thumbnail']['value'] != null) {
+                                    if ($enclosure_type == 'image/jpeg' || $enclosure_type == 'image/png' || $enclosure_type == 'image/gif') {
+                                        if ($save_images) {
+                                            $with[$info['item_thumbnail']['value']] = $this->grabImage($enclosure_link, $assetcontainer);
+                                        } else {
+                                            $with[$info['item_thumbnail']['value']] = $enclosure_link;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        // Custom terms
+                        if (isset($info['custom_terms']) && isset($info['custom_taxonomies'])) {
+                            $tags = $info['custom_terms'];
+                            $newtags = [];
+                            foreach ($tags as $term) {
+                                array_push($newtags, $term);
+                            }
+                            $with[$taxonomy] = $newtags;
+                        }
+
+                        // Create an entry
+                        if (Entry::slugExists($slugged, $publish_to)) {
+
+                            $this->info($item->get_title() . " <fg=red>already exists</>");
+
                         } else {
-                            $url = $url . '?' . http_build_query($newquery, '', '&amp;');
-                        }
-                    }
 
-                    // Add items to the chosen collection
-                    foreach ($feed->get_items() as $item) {
+                            if ($info['status'] == 'publish') {
 
-                        if ($active === true) {
+                                $this->info('Adding "' . $item->get_title() . '"');
 
-                            $with = [];
-                            $with[$info['item_title']['value']] = $item->get_title(); // Add the title
-                            $with['item_pubdate'] = $item->get_date(); // Add the pubdate
-                            $slugged = slugify($item->get_title());
-
-                            // Item description
-                            if (isset($info['item_description']) && $item->get_description()) {
-                                if ($info['item_description']['source'] != 'disable' && $info['item_description']['value'] != null) {
-                                    $with[$info['item_description']['value']] = $item->get_description();
-                                }
-                            }
-
-                            // Item content
-                            if (isset($info['item_content']) && $item->get_content()) {
-                                if ($info['item_content']['source'] != 'disable' && $info['item_content']['value'] != null) {
-                                    $with[$info['item_content']['value']] = $item->get_content();
-                                }
-                            }
-
-                            // Item permalink
-                            if (isset($info['item_permalink']) && $item->get_permalink()) {
-                                if ($info['item_permalink']['source'] != 'disable' && $info['item_permalink']['value'] != null) {
-                                    $with[$info['item_permalink']['value']] = $item->get_permalink();
-                                }
-                            }
-
-                            // Item authors
-                            if (isset($info['item_authors'])) {
-                                if ($info['item_authors']['source'] != 'disable' && $info['item_authors']['value'] != null) {
-                                    if ($author_options == 'create') {
-                                        if ($author = $feed->get_author()) {
-                                            // Create new user
-                                            User::create($author->get_name())
-                                                ->username(slugify($author->get_name()))
-                                                ->save();
-                                        }
-                                    } else {
-                                        // Assign to an existing user
-                                        $with[$info['item_authors']['value']] = $author;
-                                    }
-                                }
-                            }
-
-                            // Item thumbnails
-                            if ($enclosure = $item->get_enclosure()) {
-                                $enclosure_type = $enclosure->get_type();
-                                $enclosure_link = $enclosure->get_link();
-
-                                if (isset($info['item_thumbnail'])) {
-                                    if ($info['item_thumbnail']['source'] != 'disable' && $info['item_thumbnail']['value'] != null) {
-                                        if ($enclosure_type == 'image/jpeg' || $enclosure_type == 'image/png' || $enclosure_type == 'image/gif') {
-                                            if ($save_images) {
-                                                $with[$info['item_thumbnail']['value']] = $this->grabImage($enclosure_link, $assetcontainer);
-                                            } else {
-                                                $with[$info['item_thumbnail']['value']] = $enclosure_link;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-
-                            // Custom terms
-                            if (isset($info['custom_terms']) && isset($info['custom_taxonomies'])) {
-                                $tags = $info['custom_terms'];
-                                $newtags = [];
-                                foreach ($tags as $term) {
-                                    array_push($newtags, $term);
-                                }
-                                $with[$taxonomy] = $newtags;
-                            }
-
-                            // Create an entry
-                            if (Entry::slugExists($slugged, $publish_to)) {
-
-                                $this->info($item->get_title() . " <fg=red>already exists</>");
+                                Entry::create($slugged)
+                                    ->collection($publish_to)
+                                    ->with($with)
+                                    ->date($item->get_date('Y-m-d'))
+                                    ->save();
 
                             } else {
 
-                                if ($info['status'] == 'publish') {
+                                $this->info('Adding "' . $item->get_title() . '" <fg=red>(draft)</>');
 
-                                    $this->info('Adding "' . $item->get_title() . '"');
-
-                                    Entry::create($slugged)
-                                        ->collection($publish_to)
-                                        ->with($with)
-                                        ->date($item->get_date('Y-m-d'))
-                                        ->save();
-
-                                } else {
-
-                                    $this->info('Adding "' . $item->get_title() . '" <fg=red>(draft)</>');
-
-                                    Entry::create($slugged)
-                                        ->collection($publish_to)
-                                        ->published(false)
-                                        ->with($with)
-                                        ->date($item->get_date('Y-m-d'))
-                                        ->save();
-
-                                }
-
-                                $i++;
+                                Entry::create($slugged)
+                                    ->collection($publish_to)
+                                    ->published(false)
+                                    ->with($with)
+                                    ->date($item->get_date('Y-m-d'))
+                                    ->save();
 
                             }
 
+                            $i++;
+
                         }
 
-                        sleep(3);
-
                     }
 
-                    if ($i == 0) {
-
-                        $this->info("No new articles.");
-
-                    } else {
-
-                        $this->info("Update complete. I found " . $i . " new articles and added them to " . $publish_to . ".");
-
-                    }
-                    $this->info("\n");
+                    sleep(3);
 
                 }
+
+                if ($i == 0) {
+
+                    $this->info("No new articles.");
+
+                } else {
+
+                    $this->info("Update complete. I found " . $i . " new articles and added them to " . $publish_to . ".");
+
+                }
+                $this->info("\n");
+
             }
         }
     }
